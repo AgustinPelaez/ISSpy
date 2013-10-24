c1eb8b0925b20d1fd58684c4dbdfde07d606cc65
AgustinPelaez <agustin.pelaez@ubidots.com>
2013-10-24 15:31:28 -0400

    Latest version

--- ISSpy.py
+++ ISSpy.py
@@ -1,6 +1,6 @@
 from ubidots import ApiClient
 import requests
-
+from math import *
 
 #Get variables
 
@@ -12,10 +12,22 @@ latlong = dict['iss_position'];
 lat = latlong['latitude']
 long = latlong['longitude']
 
-print lat
+#Calculate Distance to Boston
+
+lat2 = 42.3581
+long2 = 71.0636
+
+a = sin((lat2-lat)/2)**2 + cos(lat)*cos(lat2)*sin((long2-long)/2)**2
+c = 2*atan2(sqrt(a),sqrt(1-a))
+d = 6378.1*c
 
 #Post variables
 
 api = ApiClient('a21ebaf64e14d195c0044fcc3b9f6dab9d653af3')
-latitude = api.get_variable(id = '56799cf1231b28459f976417')
-longitude = api.get_variable(id = '56799cf1231b28459f976417')
+latitude = api.get_variable(id = '525c7e4ef91b285823f85d5a')
+longitude = api.get_variable(id = '525c7e43f91b285823f85d59')
+boston_distance = api.get_variable(id = '525c7e69f91b2858265d746a')
+
+boston_distance.save_value({'value':d})
+latitude.save_value({'value':lat})
+longitude.save_value({'value':long})
