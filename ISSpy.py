29f908f16c93641dafc7ee474b1bdc10738239f7
AgustinPelaez <agustin@rhel-i386.softlayer.local>
2013-12-29 21:17:15 -0600

    ISSpy.py

--- ISSpy.py
+++ ISSpy.py
@@ -1,33 +1,35 @@
 from ubidots import ApiClient
-import requests
+import requests,time
 from math import *
+
+
+#Connect to Ubidots
+
+api = ApiClient('a21ebaf64e14d195c0044fcc3b9f6dab9d653af3')
+
+#Instantiate local variable from Ubidots
+
+boston_distance = api.get_variable('525c7e69f91b2858265d746a')
 
 #Get variables
 
-req_iss = requests.get('http://api.open-notify.org/iss-now.json')
-dict = req_iss.json()
+while(1):
+   req_iss = requests.get('http://api.open-notify.org/iss-now.json')
+   dict = req_iss.json()
+   latlong = dict['iss_position'];
+   lat = latlong['latitude']
+   long = latlong['longitude']
 
-latlong = dict['iss_position'];
+   #Calculate Distance to Boston
 
-lat = latlong['latitude']
-long = latlong['longitude']
-
-#Calculate Distance to Boston
-
-lat2 = 42.3581
-long2 = 71.0636
-
-a = sin((lat2-lat)/2)**2 + cos(lat)*cos(lat2)*sin((long2-long)/2)**2
-c = 2*atan2(sqrt(a),sqrt(1-a))
-d = 6378.1*c
-
-#Post variables
-
-api = ApiClient('a21ebaf64e14d195c0044fcc3b9f6dab9d653afx')
-latitude = api.get_variable(id = '525c7e4ef91b285823f85d5x')
-longitude = api.get_variable(id = '525c7e43f91b285823f85d5x')
-boston_distance = api.get_variable(id = '525c7e69f91b2858265d746x')
-
-boston_distance.save_value({'value':d})
-latitude.save_value({'value':lat})
-longitude.save_value({'value':long})
+   my_lat = 42.3581
+   my_long = 71.0636
+   
+   a = sin((my_lat-lat)/2)**2 + cos(lat)*cos(my_lat)*sin((my_long-long)/2)**2
+   c = 2*atan2(sqrt(a),sqrt(1-a))
+   d = 6378.1*c
+   d = round(d,1)
+   
+   #Send value to Ubidots
+   boston_distance.save_value({'value':d})
+   time.sleep(1)
